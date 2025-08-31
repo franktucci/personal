5dfe929320d59bf7798596356fce759a460d0cbf
franktucci <ftucci@calpoly.edu>
2025-08-31 16:19:35 -0700

    no padding on mobile devices

--- src/components/PageTemplate.jsx
+++ src/components/PageTemplate.jsx
@@ -3,7 +3,8 @@ import NavBar from "./NavBar";
 
 export default function PageTemplate(props) {
   return (
-    <Container fluid style={{paddingLeft:"10vw", paddingRight:"10vw", paddingBottom:"10vw"}}>
+    <>
+    <Container className="d-none d-sm-block" fluid style={{paddingLeft:"10vw", paddingRight:"10vw", paddingBottom:"10vw"}}>
       <NavBar/>
       <Card style={{borderRadius:"0", padding:"3rem", backgroundColor:"#071015", textDecorationColor:"white", boxShadow:"-20px 0 30px -3px black, 20px 0 30px -3px black"}}>
         <span style={{color:"white"}}>
@@ -11,6 +12,15 @@ export default function PageTemplate(props) {
         </span>
       </Card>
     </Container>
+    <Container className="d-sm-none" fluid style={{paddingLeft:"0", paddingRight:"0", paddingBottom:"0"}}>
+      <NavBar/>
+      <Card style={{borderRadius:"0", padding:"3rem", backgroundColor:"#071015", textDecorationColor:"white", boxShadow:"-20px 0 30px -3px black, 20px 0 30px -3px black"}}>
+        <span style={{color:"white"}}>
+          {props.children}
+        </span>
+      </Card>
+    </Container>
+    </>
   );
 };
   
